c9502bc34394b47bdf52354777228a6afa90ed83
Mack Hayden <unionmack@gmail.com>
2020-06-27 13:42:16 -0700

    added updateButton function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -17,6 +17,13 @@ function togglePlay () {
     }
 }
 
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
 //Hook up the event listeners
 video.addEventListener('click', togglePlay)
+video.addEventListener('play', updateButton)
+video.addEventListener('pause', updateButton)
 toggle.addEventListener('click', togglePlay)
